e9f7b6778ce73083e4f715ca7a4c67a373c51549
Giovanni Toraldo <scurippio@gmail.com>
2010-10-16 10:38:32 +0200

    Translation for user_privacy field.

---
+++ user_fields/user_privacy.php
@@ -0,0 +1,14 @@
+<?php
+$locale['uf_privacy'] = "Privacy Profilo";
+$locale['uf_privacy_desc'] = "Opzioni Privacy Profilo";
+$locale['uf_privacy_001'] = "Privacy Profilo <span class='side' title='header=[Privacy Profilo] body=[Consente di decidere chi pu&ograve; visualizzare il proprio profilo utente.]'>(?)</span>";
+$locale['uf_privacy_002'] = "<span class='side' title='header=[Everyone] body=[Il tuo profilo &egrave; liberamente accessibile.]'>Pubblico</span>";
+$locale['uf_privacy_003'] = "<span class='side' title='header=[Members Only] body=[Solo gli utenti registrati a questo sito possono visualizzare il profilo.]'>Solo altri utenti registrati</span>";
+$locale['uf_privacy_004'] = "Non definito";
+$locale['uf_privacy_005'] = "<span class='side' title='header=[Admins Only] body=[Solo gli amministratori possono accedere al profilo.]'>Solo Amministratori</span>";
+$locale['uf_privacy_006'] = "Questo profilo non &egrave; pubblico!<br>Puoi effettuare il <a href='".BASEDIR."login.php' class='side'>Login</a> oppure <a href='".BASEDIR."register.php' class='side'>Registrati</a> per accedere a questo profilo.";
+$locale['uf_privacy_007'] = "Privacy Profilo";
+$locale['uf_privacy_008'] = "Questo profilo non &egrave; attualmente disponibile!";
+$locale['uf_privacy_009'] = "Solo altri utenti registrati";
+$locale['uf_privacy_010'] = "Solo Amministratori";
+?>
