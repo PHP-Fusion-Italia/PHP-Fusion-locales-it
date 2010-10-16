fe29207af0bbb826bcaf64b6f70740c665e7ec8b
Giovanni Toraldo <scurippio@gmail.com>
2010-10-16 10:57:32 +0200

    Translation for user_version field.

---
+++ user_fields/user_version.php
@@ -0,0 +1,6 @@
+<?php
+$locale['uf_ver'] = "Versione PHP-Fusion";
+$locale['uf_verb'] = "Versione PHP-Fusion: <span class='small2'>(x.xx.xx)</span>";
+$locale['uf_ver2'] = "<img style='vertical-align:middle; border:0;' src='".INCLUDES."user_fields/images/fusion.png' alt=''  /> ";
+$locale['uf_ver_desc'] = "Versione PHP-Fusion";
+?>
