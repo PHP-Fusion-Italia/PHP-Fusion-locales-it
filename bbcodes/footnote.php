c40bdd713d17bc9ddec91d3ef865ead0dc6adb47
Giovanni gionn Toraldo <scorp@php-fusion.it>
2011-04-19 21:29:19 +0200

    Aggiunte traduzioni per footnote e offtopic bbcode

---
+++ bbcodes/footnote.php
@@ -0,0 +1,5 @@
+<?php
+//XXX footnote
+$locale['bb_footnote_description'] = "footnote";
+$locale['bb_footnote_usage'] = "footnote";
+?>
---
+++ bbcodes/offtopic.php
@@ -0,0 +1,5 @@
+<?php
+//XXX offtopic
+$locale['bb_offtopic_description'] = "offtopic";
+$locale['bb_offtopic_usage'] = "offtopic";
+?>
