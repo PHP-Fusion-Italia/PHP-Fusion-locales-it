4d69a366cee7082604d1b5ca16a4613479f6c179
Giovanni gionn Toraldo <scorp@php-fusion.it>
2011-04-19 21:31:10 +0200

    Aggiunto bbcode chili

---
+++ bbcodes/chili.php
@@ -0,0 +1,13 @@
+<?php
+//chili 
+$locale['chili_01'] = "Chili 2.2 evidenziatore di codice"; 
+$locale['chili_02'] = "Nome del linguaggio"; 
+$locale['chili_03'] = "Codice da evidenziare con Chili 2.2"; 
+$locale['chili_04'] = "Mostra";
+$locale['chili_05'] = "Nascondi";
+$locale['chili_06'] = "Elaborato in ";
+$locale['chili_07'] = " millisecondi, utilizzando Chili 2.2";
+$locale['chili_08'] = "Fai click e attendi mentre si carica il codice...";
+$locale['chili_09'] = "Scarica sorgente";
+$locale['chili_10'] = "Codice";
+?>
